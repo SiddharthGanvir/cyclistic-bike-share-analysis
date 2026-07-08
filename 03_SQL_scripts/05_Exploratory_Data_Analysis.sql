ef9becb517647cd89f33e0bc0b37300d1feff5e7
Siddharth Ganvir <siddharthganvir05@gmail.com>
2026-07-09 01:01:51 +0530

    Added few modifications in exploratory data analysis

--- 03_SQL_scripts/05_Exploratory_Data_Analysis.sql
+++ 03_SQL_scripts/05_Exploratory_Data_Analysis.sql
@@ -69,3 +69,37 @@ member_casual,
 rideable_type
 ORDER BY total_rides DESC;
 
+------- Percentage contribution of each rideable type by member type --------
+SELECT
+    outer_table.member_casual,
+    outer_table.rideable_type,
+    COUNT(*) AS total_rides,
+
+    ROUND(
+        (COUNT(*) * 100.0) /
+        (
+            SELECT COUNT(*)
+            FROM bike_trips_features AS inner_table
+            WHERE inner_table.member_casual =
+                  outer_table.member_casual
+        ),
+        2
+    ) AS ride_percentage
+
+FROM bike_trips_features AS outer_table
+
+GROUP BY
+    outer_table.member_casual,
+    outer_table.rideable_type
+
+ORDER BY
+    outer_table.member_casual,
+    total_rides DESC;
+
+------- Average ride durations by member type -------
+SELECT
+    member_casual,
+    ROUND(AVG(ride_length_minutes), 2) AS avg_ride_duration_minutes
+FROM bike_trips_features
+GROUP BY member_casual
+ORDER BY avg_ride_duration_minutes DESC;

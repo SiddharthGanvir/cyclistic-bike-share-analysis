d564a226db409931f0b2214ecb301d2c42545df5
Siddharth Ganvir <siddharthganvir05@gmail.com>
2026-07-12 18:45:44 +0530

    Completed SQL data analysis for Cyclistic bike share project

--- 03_SQL_scripts/05_Exploratory_Data_Analysis.sql
+++ 03_SQL_scripts/05_Exploratory_Data_Analysis.sql
@@ -103,3 +103,140 @@ SELECT
 FROM bike_trips_features
 GROUP BY member_casual
 ORDER BY avg_ride_duration_minutes DESC;
+
+------- Monthly Ride Trends ---------
+SELECT
+    month_number,
+    COUNT(*) AS total_rides,
+    ROUND(
+        (COUNT(*) * 100.0) /
+        (
+            SELECT COUNT(*)
+            FROM bike_trips_features
+        ),
+        2
+    ) AS ride_percentage
+FROM bike_trips_features
+GROUP BY month_number
+ORDER BY month_number;
+
+-------- Monthly ride trends between members and casual riders ------
+SELECT
+    month_number,
+    member_casual,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+GROUP BY
+    month_number,
+    member_casual
+ORDER BY
+    month_number,
+    member_casual;
+
+------- Day of week trend analysis --------
+SELECT
+    day_of_week_number,
+    COUNT(*) AS total_rides,
+    ROUND(
+        (COUNT(*) * 100.0) /
+        (
+            SELECT COUNT(*)
+            FROM bike_trips_features
+        ),
+        2
+    ) AS ride_percentage
+FROM bike_trips_features
+GROUP BY day_of_week_number
+ORDER BY day_of_week_number;
+
+-------- Members vs casual riders day analysis -------
+SELECT
+    day_of_week_number,
+    member_casual,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+GROUP BY
+    day_of_week_number,
+    member_casual
+ORDER BY
+    day_of_week_number,
+    member_casual;
+
+-------- Hour of week analysis -------
+SELECT
+    hour_of_day,
+    COUNT(*) AS total_rides,
+    ROUND(
+        (COUNT(*) * 100.0) /
+        (
+            SELECT COUNT(*)
+            FROM bike_trips_features
+        ),
+        2
+    ) AS ride_percentage
+FROM bike_trips_features
+GROUP BY hour_of_day
+ORDER BY hour_of_day;
+
+------ Hour of week analysis by member type -------
+SELECT
+    hour_of_day,
+    member_casual,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+GROUP BY
+    hour_of_day,
+    member_casual
+ORDER BY
+    hour_of_day,
+    member_casual;
+
+-------- Weekday vs Weekend Analysis --------
+SELECT
+    is_weekend,
+    COUNT(*) AS total_rides,
+    ROUND(
+        (COUNT(*) * 100.0) /
+        (
+            SELECT COUNT(*)
+            FROM bike_trips_features
+        ),
+        2
+    ) AS ride_percentage
+FROM bike_trips_features
+GROUP BY is_weekend
+ORDER BY is_weekend;
+
+------- Weekday vs Weekend Rides Trend by member types -------
+SELECT
+    is_weekend,
+    member_casual,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+GROUP BY
+    is_weekend,
+    member_casual
+ORDER BY
+    is_weekend,
+    member_casual;
+
+
+------- Top Start Stations -------
+SELECT
+    start_station_name,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+WHERE start_station_name IS NOT NULL
+GROUP BY start_station_name
+ORDER BY total_rides DESC
+LIMIT 10;
+
+------- Top End Stations --------
+SELECT
+    end_station_name,
+    COUNT(*) AS total_rides
+FROM bike_trips_features
+WHERE end_station_name IS NOT NULL
+GROUP BY end_station_name
+ORDER BY total_rides DESC
+LIMIT 10;
